7a0de6b02bbef13ff121e831168a823229d1a026
Chinmay-Kumar-Sahoo <162298205+Chinmay-Kumar-Sahoo@users.noreply.github.com>
2024-03-06 21:05:36 +0000

    Conversion of datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountID = 145862
+let accountEmail = "chinmay@google.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+
+accountCity = "Mumbai"
+//accountID = 21
+
+accountEmail = "jhfwf@google.com"
+accountPassword = "444245"
+
+console.table([accountID, accountEmail, accountPassword, accountCity]);
+
+/*console.log(accountID);
+console.log(accountCity);
+*/
---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,14 @@
+let score = "string"
+/* console.log(typeof score);
+console.log(typeof(score));
+*/
+let valueinNumber = Number(score)
+console.log(valueinNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1, false => 0
+
+let loggedin = 1
+let booleanloggedin = Boolean(loggedin)
+console.log(booleanloggedin);
